5334bd3d42680b151106a9f362cb4db011d87846
manoj hc <manojhc.dev@gmail.com>
2025-07-19 18:07:31 +0530

    select_statement

---
+++ 2.Select_statement.sql
@@ -0,0 +1,14 @@
+SELECT first_name,
+last_name,
+birth_date
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name,
+employee_id,
+salary
+FROM parks_and_recreation.employee_salary;
+
+
+
+
+
